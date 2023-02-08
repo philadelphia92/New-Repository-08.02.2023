703d76496ae39f6d2236cdfc9c2798450afc6117
Igor <exetto92@gmail.com>
2023-02-08 17:52:06 +0300

    Task 36

---
+++ 036/Program.cs
@@ -0,0 +1,15 @@
+﻿// 36. Написать программу вычисления произведения чисел от 1 до N
+
+System.Console.Write("Введите число - ");
+string write=Console.ReadLine();
+int N=Convert.ToInt32(write);
+int NumbersComposition(int a)
+{
+    int compos=1;
+    for(int num=1; num<=a; num++)
+    {
+        compos=compos*num;
+    }
+    return compos;
+}
+System.Console.WriteLine($"Произведение чисел от 1 до {N} = {NumbersComposition(N)}");
