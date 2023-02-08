f33d4e4bc464ead7e89341ab0db8ae04d1fc9c3a
Igor <exetto92@gmail.com>
2023-02-08 17:43:23 +0300

    Task 35

---
+++ 035/Program.cs
@@ -0,0 +1,12 @@
+﻿// 35. Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+int number=12345;
+int sumDigit=0;
+System.Console.Write($"Сумма цифр числа {number} = ");
+while (number>0)
+{
+    int digit=number%10;
+    sumDigit=sumDigit+digit;
+    number=number/10;
+}
+System.Console.WriteLine(sumDigit);
