a8b590a94346cd7a38603d5ef4f4dcf0239ca7b0
Igor <exetto92@gmail.com>
2023-02-08 17:37:50 +0300

    Task 33

---
+++ 033/Program.cs
@@ -0,0 +1,12 @@
+﻿// 33. Возведите число А в натуральную степень B используя цикл
+
+int number=5;
+int exponent =15;
+int count =1;
+int result=number;
+while (count<exponent)
+{
+    result=result*number;
+    count++;
+}
+System.Console.WriteLine($"{number}^{exponent}={result}");
