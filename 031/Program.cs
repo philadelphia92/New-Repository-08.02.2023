f838e279a40f255ec23e35a0973157e0af05943e
Igor <exetto92@gmail.com>
2023-02-08 17:29:26 +0300

    Task 31

---
+++ 031/Program.cs
@@ -0,0 +1,9 @@
+﻿// 31. Вывести на экран кубы чисел от 1 до N
+
+int a = 12345;
+int b = 123;
+ 
+int c = (int) Math.Pow(a, 1.0/3.0);
+int i3;
+for (int i = c; (i3 = i * i * i) >= b; i--)
+   Console.WriteLine($"{i}^3 = {i3}");
