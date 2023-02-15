7046407f74bc58f4451aec53666e5d008eefadf6
Igor <exetto92@gmail.com>
2023-02-15 22:36:29 +0300

    Task 51

---
+++ 051/Program.cs
@@ -0,0 +1,17 @@
+﻿// 51. С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("Введите числа через пробел: ");
+
+int[] array=Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int Numbers(int[] array)
+{
+    int count=0;
+
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i]>0) count++;
+    }
+    return count;
+}
+Console.WriteLine(Numbers(array));
