ac7de9d85ba56326b75a7df837493ebb7828e1cd
Igor <exetto92@gmail.com>
2023-02-15 22:51:52 +0300

    Task 56

---
+++ 056/Program.cs
@@ -0,0 +1,18 @@
+﻿// 56. Написать программу копирования массива
+
+int[] array=new int[10];
+int arrayCopy;
+
+Console.Write("Original ");
+for(int i=0; i<array.Length; i++)
+{
+    array[i]=new Random().Next(0, 50);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine(); 
+Console.Write("Copy ");
+for(int i=0; i<array.Length; i++)
+{
+    arrayCopy=array[i];
+    Console.Write(arrayCopy + " ");
+}
