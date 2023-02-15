a987478b6c21c2c63fe5f6aced135cea242cd497
Igor <exetto92@gmail.com>
2023-02-15 22:17:44 +0300

    Task 41

---
+++ 041/Program.cs
@@ -0,0 +1,20 @@
+﻿// 41. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int N = 8;
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+PrintArray(SetArray(N));
