d59d4a45472f57b2ef72d0e41a08414c6f258c7e
Igor <exetto92@gmail.com>
2023-02-19 22:55:57 +0300

    Task 68

---
+++ 068/Program.cs
@@ -0,0 +1,29 @@
+﻿// 68. Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] array=new int[5, 6];
+
+for(int i=0; i<array.GetLength(0); i++)
+{
+    for(int j=0; j<array.GetLength(1); j++)
+    {
+        array[i, j]=new Random().Next(1, 20);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int m=0;
+for(int i=0; i<array.GetLength(1); i++)
+{
+    m=array[array.GetLength(0) - 1, i];
+    array[array.GetLength(0) - 1, i]=array[0, i];
+    array[0, i]=m;
+}
+for(int i=0; i<array.GetLength(0); i++)
+{
+    for(int j=0; j<array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
