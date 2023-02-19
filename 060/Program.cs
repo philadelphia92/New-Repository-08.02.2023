e20e517bbe14f2422c4a1d7f0871f3e8366183f0
Igor <exetto92@gmail.com>
2023-02-19 22:41:36 +0300

    Task 60

---
+++ 060/Program.cs
@@ -0,0 +1,32 @@
+﻿// 60. Показать двумерный массив размером m×n заполненный целыми случайными числами
+
+void FillArray(int[,] array)                                          
+{
+    for(int m=0; m<array.GetLength(0); m++)
+    {
+        for(int n=0; n<array.GetLength(1); n++)
+        {
+            array[m, n]=new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for(int m=0; m<array.GetLength(0); m++)
+    {
+        for(int n=0; n<array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m=5;
+int n=4;
+
+int[,] array=new int[m, n];
+
+FillArray(array);
+PrintArray(array);
