e990c6aa5ba81bdad176a13f627517a3e88122c2
Igor <exetto92@gmail.com>
2023-02-19 22:52:28 +0300

    Task 64

---
+++ 064/Program.cs
@@ -0,0 +1,45 @@
+﻿// 64. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты по значению.
+
+void FillArray(int[,] array)
+{
+    for(int m=0; m<array.GetLength(0); m++)
+    {
+        for(int n=0; n<array.GetLength(1); n++)
+        {
+            array[m, n]=new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for(int m=0; m<array.GetLength(0); m++)
+    {
+        for(int n=0; n<array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Replace(int[,] array)
+{
+ 
+    for(int m=0; m<array.GetLength(0); m += 2)
+    {
+        for(int n=0; n<array.GetLength(1); n += 2)
+        {
+            array[m, n]=array[m, n]*array[m, n];
+        }
+    }
+}
+
+int m=5;
+int n=5;
+
+int[,] array=new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Replace(array);
+PrintArray(array);
