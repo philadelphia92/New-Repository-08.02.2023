cc98161085808a48030b9ff18eac0ffa5b5ef08a
Igor <exetto92@gmail.com>
2023-02-19 22:47:04 +0300

    Task 62

---
+++ 062/Program.cs
@@ -0,0 +1,44 @@
+﻿// 62. В двумерном массиве n×k заменить четные элементы на противоположные по значению (a[i,j]=-a[i,j]). Написать отдельную подпрограмму на вход которой подается массив, а на выходе измененный массив.
+
+void FillArray(int[,] array)
+{
+    for(int n=0; n<array.GetLength(0); n++)
+    {
+        for(int k=0; k<array.GetLength(1); k++)
+        {
+            array[n, k]=new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for(int n=0; n<array.GetLength(0); n++)
+    {
+        for(int k=0; k<array.GetLength(1); k++)
+        {
+            Console.Write($"{array[n, k]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] array)
+{
+    for(int n=0; n<array.GetLength(0); n++)
+    {
+        for(int k=0; k<array.GetLength(1); k++)
+        {
+            if(array[n,k] % 2 == 0) array[n, k]=-array[n, k];
+        }
+    }
+}
+int n=5;
+int k=4;
+
+int[,] array=new int[n, k];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
