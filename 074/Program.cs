76c7485ad4453dfb3e03889d9c55a045454ae092
Igor <exetto92@gmail.com>
2023-02-22 22:18:12 +0300

    Task 74

---
+++ 074/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int SumDigits(int number)
+{
+    if(number<1) return 0;
+    int result=0;
+    return SumDigits(number/10)+number%10;
+}
+Console.Write("Введите номер: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sum digits: {SumDigits(number)}");
