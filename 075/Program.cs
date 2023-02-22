c81e4425ceb05fb3183f2796cb5c772b4e75f92a
Igor <exetto92@gmail.com>
2023-02-22 22:22:24 +0300

    Task 75

---
+++ 075/Program.cs
@@ -0,0 +1,20 @@
+﻿// 75. Написать программу вычисления функции Аккермана
+
+int Ack(int m, int n)
+{
+    if(m>6 || n>6 || m<0 || n<0) return-1;
+
+    if(m==0)
+    {
+        return n+1;
+    }
+    else if(n==0)
+    {
+        return Ack(m-1,1);
+    }
+    else
+    {
+        return Ack(m-1,Ack(m,n-1));
+    }
+}
+Console.WriteLine(Ack(2, 3));
