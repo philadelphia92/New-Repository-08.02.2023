d2478a5d696c0582d9fa60ebf7c9ee003a50af57
Igor <exetto92@gmail.com>
2023-02-27 19:50:15 +0300

    Task 82

---
+++ 082/Program.cs
@@ -0,0 +1,14 @@
+﻿// 62. C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+
+Console.Write("Введите значение: ");
+string str = Console.ReadLine();
+int j = 0;
+foreach(char q in str)
+{
+    if(q != '.') j++;
+    else break;
+}
+if(j == str.Length)
+    System.Console.WriteLine("В строке нет точки");
+else
+    System.Console.WriteLine($"Количество символов до точки: {j}");
